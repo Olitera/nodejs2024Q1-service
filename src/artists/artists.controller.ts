cf6bbea35342e60ca498fa81490bcb7f58a72c72
Volha Melayok <neiwra@gmail.com>
2024-03-10 19:18:11 +0100

    feat: add artists methods

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,6 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ArtistsService } from '../artists/artists.service';
+import { Artist } from '../interfaces/artists.interface';
 
 @Controller('artist')
 export class ArtistsController {
+
+  constructor(private artistsService: ArtistsService) {}
+
+  @Post()
+  createArtist() {
+    return this.artistsService.createArtist();
+  }
+
+  @Get()
+  getAllArtists() {
+    return this.artistsService.getAllArtists();
+  }
+
+  @Get(':id')
+  getArtistById(@Param('id') id: string) {
+    return this.artistsService.getArtistById(id);
+  }
+
+  @Put(':id')
+  updateArtistInfo(@Param('id') id: string, @Body() body: Artist) {
+    return this.artistsService.updateArtistInfo(id, body);
+  }
+
+  @Delete(':id')
+  deleteArtist(@Param('id') id: string) {
+    return this.artistsService.deleteArtistById(id)
+  }
 
 }
--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,7 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { Artist } from '../interfaces/artists.interface';
+import { v4 as uuidV4 } from 'uuid';
 
 @Injectable()
 export class ArtistsService{
   private artists: Artist[] = [];
+
+  createArtist() {
+    const artist: Artist = {
+      grammy: false,
+      id: uuidV4(),
+      name: ''
+    };
+    this.artists.push(artist);
+    return artist;
+  }
+
+  getAllArtists() {
+    return this.artists;
+  }
+
+  getArtistById(id: string) {
+    return this.artists.find((artist) => artist.id === id);
+  }
+
+  updateArtistInfo(id: string, artistNewInfo: Artist) {
+    const artist = this.artists.find((artist) => artist.id === id);
+    if(artist) {
+      artist.name = artistNewInfo.name;
+      artist.grammy = artistNewInfo.grammy;
+    }
+    return artist
+  }
+
+  deleteArtistById(id: string) {
+    const artist = this.artists.find((artist) => artist.id === id);
+    if(artist) {
+      this.artists = this.artists.filter((artist) => artist.id !== id);
+    }
+  }
 }
